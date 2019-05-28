81161845f4366af9602f9f4985271db14e3bf888
yanev.petar@gmail.com <yanev.petar@gmail.com>
2019-05-28 13:48:02 +0300

    Separed details business page

---
+++ AlphaHotel/Controllers/BusinessController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaHotel.Controllers
+{
+    public class BusinessController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
