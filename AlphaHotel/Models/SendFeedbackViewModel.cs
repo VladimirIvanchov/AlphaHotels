7bd3c1a235c401d8fbb64f4834e41ede48d8ea70
Vlado <vladimirivanchov@gmail.com>
2019-06-06 00:27:48 +0300

    Added FeedbackView

---
+++ AlphaHotel/Models/SendFeedbackViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlphaHotel.Models
+{
+    public class SendFeedbackViewModel
+    {
+        public int BusinessId { get; set; }
+
+        public int Rating { get; set; }
+
+        [MaxLength(20)]
+        public string Author { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string FeedbackText { get; set; }
+    }
+}
---
+++ AlphaHotel/wwwroot/js/send-feedback.js
@@ -0,0 +1,39 @@
+﻿$('#send-feedback-form').on('submit', function (ev) {
+    ev.preventDefault();
+
+    var $this = $(this);
+
+    var url = window.location.pathname;
+    var id = url.substring(url.lastIndexOf('/') + 1);
+    var attrUrl = $this.attr('action');
+
+    var dataToSend = $this.serialize();
+    dataToSend = dataToSend + '&BusinessId=' + id;
+
+    var isValid = $this.valid();
+
+    if (isValid) {
+        $.post(attrUrl, dataToSend, function (response) {
+            $(function () {
+                $('#exampleModal').modal('toggle');
+
+            });
+
+        }).fail(function (error) {
+
+        });
+    }
+    //else {
+    //    toastr.fail(error);
+    //}
+});
+
+$('#exampleModal').on('hidden.bs.modal', function (e) {
+    $(this)
+        .find("input,textarea,select")
+        .val('')
+        .end()
+        .find("input[type=checkbox], input[type=radio]")
+        .prop("checked", "")
+        .end();
+})
