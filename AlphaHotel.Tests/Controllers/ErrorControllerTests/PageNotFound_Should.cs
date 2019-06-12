40dac7502bcbc003d328ef4c59bd2fbbb16fcd63
yanev.petar@gmail.com <yanev.petar@gmail.com>
2019-06-12 18:47:21 +0300

    Added ErrorControllerTests

---
+++ AlphaHotel.Tests/Controllers/ErrorControllerTests/PageNotFound_Should.cs
@@ -0,0 +1,20 @@
+﻿using AlphaHotel.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlphaHotel.Tests.Controllers.ErrorControllerTests
+{
+    [TestClass]
+    public class PageNotFound_Should
+    {
+        [TestMethod]
+        public void ReturnCorrectViewResult()
+        {
+            var errorController = new ErrorController();
+
+            var result = errorController.PageNotFound() as ViewResult;
+
+            Assert.AreEqual(result.ViewName, "PageNotFound");
+        }
+    }
+}
