2ef8a206ad75562fdd52d99390de0a86bf7f61ea
yanev.petar@gmail.com <yanev.petar@gmail.com>
2019-06-13 14:50:46 +0300

    Revert "Revert "Added BusinessControllerTests""
    This reverts commit 24ac8577e20dba3ddeeab0ced54b75b7f36f6580.

---
+++ AlphaHotel.Tests/Areas/Admin/Controllers/BusinessControllerTests/AllBusiness_Should.cs
@@ -0,0 +1,53 @@
+﻿using AlphaHotel.Areas.Admin.Controllers;
+using AlphaHotel.Common;
+using AlphaHotel.DTOs;
+using AlphaHotel.Infrastructure.MappingProviders;
+using AlphaHotel.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AlphaHotel.Tests.Areas.Admin.Controllers.BusinessControllerTests
+{
+    [TestClass]
+    public class AllBusiness_Should
+    {
+        [TestMethod]
+        public async Task CallBusinessServiceOnce()
+        {
+            var businessServiceMocked = new Mock<IBusinessService>();
+            var mapperMocked = new Mock<IMappingProvider>();
+            var facilityServiceMocked = new Mock<IFacilityService>();
+            var pictureHelper = new Mock<IPictureHelper>();
+
+            var businessController = new BusinessController(businessServiceMocked.Object,
+                mapperMocked.Object, facilityServiceMocked.Object, pictureHelper.Object);
+
+            await businessController.AllBusiness();
+
+            businessServiceMocked.Verify(a => a.ListAllBusinessesAsync<BusinessDTO>(), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task ReturnCorrectViewModel()
+        {
+            var businessServiceMocked = new Mock<IBusinessService>();
+            var mapperMocked = new Mock<IMappingProvider>();
+            var facilityServiceMocked = new Mock<IFacilityService>();
+            var pictureHelper = new Mock<IPictureHelper>();
+            var businessDTOs = new List<BusinessDTO> { new BusinessDTO() };
+
+            businessServiceMocked.Setup(b => b.ListAllBusinessesAsync<BusinessDTO>())
+                .ReturnsAsync(businessDTOs);
+
+            var businessController = new BusinessController(businessServiceMocked.Object,
+                mapperMocked.Object, facilityServiceMocked.Object, pictureHelper.Object);
+
+            var result = await businessController.AllBusiness() as PartialViewResult;
+
+            Assert.AreEqual(result.ViewName, "_AllBusinessPartial");
+        }
+    }
+}
---
+++ AlphaHotel.Tests/Areas/Admin/Controllers/BusinessControllerTests/CreateBusinessGet_Should.cs
@@ -0,0 +1,58 @@
+﻿using AlphaHotel.Areas.Admin.Controllers;
+using AlphaHotel.Areas.Admin.Models;
+using AlphaHotel.Common;
+using AlphaHotel.DTOs;
+using AlphaHotel.Infrastructure.MappingProviders;
+using AlphaHotel.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AlphaHotel.Tests.Areas.Admin.Controllers.BusinessControllerTests
+{
+    [TestClass]
+    public class CreateBusinessGet_Should
+    {
+        [TestMethod]
+        public async Task CallFacilityServiceOnce()
+        {
+            var businessServiceMocked = new Mock<IBusinessService>();
+            var mapperMocked = new Mock<IMappingProvider>();
+            var facilityServiceMocked = new Mock<IFacilityService>();
+            var pictureHelper = new Mock<IPictureHelper>();
+
+            var businessController = new BusinessController(businessServiceMocked.Object,
+                mapperMocked.Object, facilityServiceMocked.Object, pictureHelper.Object);
+
+            await businessController.CreateBusiness();
+
+            facilityServiceMocked.Verify(f => f.ListAllFacilitiesAsync(), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task ReturnCorrectViewModel()
+        {
+            var businessServiceMocked = new Mock<IBusinessService>();
+            var mapperMocked = new Mock<IMappingProvider>();
+            var facilityServiceMocked = new Mock<IFacilityService>();
+            var pictureHelper = new Mock<IPictureHelper>();
+            var facilityDTOs = new List<FacilityDTO> { new FacilityDTO() };
+            var createBusinessVM = new CreateBusinessViewModel();
+
+            facilityServiceMocked.Setup(f => f.ListAllFacilitiesAsync())
+                .ReturnsAsync(facilityDTOs);
+
+            mapperMocked.Setup(m => m.MapTo<CreateBusinessViewModel>(facilityDTOs))
+                .Returns(createBusinessVM);
+
+            var accountController = new BusinessController(businessServiceMocked.Object,
+                mapperMocked.Object, facilityServiceMocked.Object, pictureHelper.Object);
+
+            var result = await accountController.CreateBusiness() as ViewResult;
+
+            Assert.IsInstanceOfType(result.Model, typeof(CreateBusinessViewModel));
+        }
+    }
+}
